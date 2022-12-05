40b601a9ff208b9f37ccd62fb1120b7e93736f22
Fivysafitri24 <89623457+fivysafitri24@users.noreply.github.com>
2022-12-05 12:35:51 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/formatDate');
+const path = require('path');
+const { User, Milestone, Post } = require('./models');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+// switch the render engine to the handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(port, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
+});
+
